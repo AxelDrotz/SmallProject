6b5658cffc95ee04cc7e185522a446d13b13a6cf
SushiQ <dnordm@kth.se>
2021-12-28 15:33:42 +0100

    Update PCAImage.py

--- PCAImage.py
+++ PCAImage.py
@@ -4,19 +4,26 @@ import random
 from sklearn.decomposition import PCA
 from sklearn import preprocessing
 
-file = np.load("plains.npy")
+dataMatrix = np.load("image_array.npy")
+
+A = dataMatrix
+# Standardizing
+A = (A - np.mean(A)) / np.std(A)
+
+print("Standardized:", A)
+print("DataMatrix has the shape:", A.shape)
 
 """
-dataMatrix is a m x n matrix
-U = mxm matrix where the coulmns are eigenvectors
+dataMatrix is a n x d matrix
+U = nxn matrix where the coulmns are eigenvectors
 """
-U, s, VT = np.linalg.svd(dataMatrix)
+U, s, VT = np.linalg.svd(A)
 
 # create m x n Sigma matrix
 Sigma = np.zeros((A.shape[0], A.shape[1]))
 # populate Sigma with n x n diagonal matrix
 Sigma[:A.shape[0], :A.shape[0]] = np.diag(s)
-# select
+# Select eigenvectors for reduction, n_elements is the new dimension
 n_elements = 2
 Sigma = Sigma[:, :n_elements]
 VT = VT[:n_elements, :]
@@ -25,6 +32,5 @@ dataMatrix2 = U.dot(Sigma.dot(VT))
 print(dataMatrix2)
 # transform
 T = U.dot(Sigma)
-print(T)
-T = A.dot(VT.T)
+# or T = A.dot(VT.T)
 print(T)
