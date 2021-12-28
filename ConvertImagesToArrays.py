1c951dd8b6e4c059ba38200d411793bc698bf9bd
AxelDrotz <axeldr@kth.se>
2021-12-28 15:02:44 +0100

    Converting images to values
    Adding plains.txt, 800 plain images. Values are added each plane is represented with 2500 rows with a value

---
+++ ConvertImagesToArrays.py
@@ -0,0 +1,36 @@
+import numpy as np
+from PIL import Image
+
+saved_arrays = []
+for k in range(1,801):
+    if k <10:
+        x ='000'+str(k)
+    if 10<=k and k<100:
+        x ='00'+str(k)
+    if k>=100:
+        x ='0'+str(k)
+
+    image = Image.open("C:/SmallProjectAdvML/SmallProject\planes/"+ 'gray'+x+'.jpg')
+    image_array = np.array(image)
+    dims = image.size
+    print(dims)
+    row = dims[0]//2
+    col = dims[1]//2
+    row = row-25
+    col = col -25
+    v=[]
+    print(row)
+    print(col)
+    for r in range(row,row+50):
+        for c in range(col,col+50):
+            v.append(image_array[c][r])
+
+    saved_arrays.append(v)
+a_file = open("plains.txt", "w")
+for i in range(len(saved_arrays)):
+    temp_sum = sum(saved_arrays[i])
+    saved_arrays[i] = saved_arrays[i]/temp_sum
+for row in saved_arrays:
+    print(sum(row))
+    np.savetxt(a_file, row)
+a_file.close()
