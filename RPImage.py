a9b596aefdc090cebfaa07b335369cfa786aa39f
Paulo Hernández <pauloh@kth.se>
2021-12-29 15:21:56 +0100

    Euclidian Distance calculation, v1

--- RPImage.py
+++ RPImage.py
@@ -1,5 +1,6 @@
 import numpy
 import pandas as pd
+import random
 
 #create random matrix
 d = 2500
@@ -37,3 +38,16 @@ for i in R:
 #print(XRP)
 #Euclidean distance
 #|| x1 -x2|| approximated by sqrt(d/k)||Rx1-Rx2|| where x1 and x2 are vectors
+
+#Create vector of 100 random indexes in the range from 0-799, where each index corresponds to a reference vector.  1x100
+
+RanRefV = random.sample(range(800), 100)
+absoluteOriginal = []
+
+for i in range(800): #Iterate over all 800 vectors
+    for j in range(100): #Iterate over the indexes of the reference vectors
+        #Substract Vector i - Reference vector. 
+        absoluteOriginal[j] = numpy.sqrt(numpy.matmul(numpy.subtract(X[i],X[RanRefV[j]]), numpy.subtract(X[i],X[RanRefV[j]])))
+
+
+    
