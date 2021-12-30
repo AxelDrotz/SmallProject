3c58a754e7ed066d77f663aeb5d9afdf7c82afe2
Ekarlstr <42819523+Ekarlstr@users.noreply.github.com>
2021-12-30 04:07:36 +0100

    Update DCT.py
    implemented the inverse of the DCT, which seems very similar..?

--- DCT.py
+++ DCT.py
@@ -33,6 +33,37 @@ def dct(matrix):
 
     return transformed_matrix
 
+def inv_dct(matrix):
+    """Performs the inverse discrete cosine transform on a matrix in the cosine-space."""
+
+    inv_transformed_matrix = np.zeros((n, m))
+
+    for i in range(m):
+        for j in range(n):
+            if i == 0:
+                ci = 1/np.sqrt(m)
+            else:
+                ci = np.sqrt(2/m)
+            if j == 0:
+                cj = 1/np.sqrt(n)
+            else:
+                cj = np.sqrt(2/n)
+
+            summation = 0
+
+            for k in range(m):
+                for l in range(n):
+                    signal = matrix[k][l] \
+                             * np.cos((2*k + 1) * i * np.pi / (2*m)) \
+                             * np.cos((2*l + 1) * j * np.pi / (2*n))
+                    summation += signal
+
+            inv_transformed_matrix[i][j] = ci * cj * summation
+
+    return inv_transformed_matrix
+
+
+
 
 
 def main():
