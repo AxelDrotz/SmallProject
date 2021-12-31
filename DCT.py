de986b877f3320683dbc628217cabaece3d09288
Ekarlstr <42819523+Ekarlstr@users.noreply.github.com>
2021-12-31 16:58:54 +0100

    Update DCT.py
    Now need to code the union of the individual blocks.

--- DCT.py
+++ DCT.py
@@ -3,7 +3,7 @@ import numpy as np
 m = 50
 n = 50
 
-section_size = 10
+section_size = 5
 
 N = n // section_size
 
@@ -22,6 +22,11 @@ def create_T():
 
 
 def main():
+    # Defining the quantization matrix from
+    # https://www.researchgate.net/figure/Labelling-of-DCT-coefficients-of-5x5-image-block_fig1_224401255:
+    Q = np.matrix([[0, 1, 5, 6, 14], [2, 4, 7, 13, 15], [3, 8, 12, 16, 21], [9, 11, 17, 20, 22], [10, 18, 19, 23, 24]])
+
+
     # Importing the data:
     vector_vals = np.load('datafile_name')
     pic_matrix = np.reshape(vector_vals, (n, m))
@@ -41,8 +46,21 @@ def main():
             blocks[i][j] = M[i*section_size:(i+1)*section_size, j*section_size:(j+1)*section_size].copy()
             Ds[i][j] = np.matmul(T, np.matmul(blocks[i][j], T.transpose()))
 
-    # We now have the blocks in cosine-space. We now need the quantization matrix.
+    # We now have the blocks in cosine-space, and need to project them on the quantization matrix.
+    Cs = blocks.copy()
+    Rs = blocks.copy()
+    Ns = blocks.copy()
+    for i in range(N):
+        for j in range(N):
+            M = Ds[i][j]
+            for iprim in range(section_size):
+                for jprim in range(section_size):
+                    Cs[i][j][iprim][jprim] = round(M[iprim][jprim]/Q[iprim][jprim])
+                    Rs[i][j][iprim][jprim] = Q[iprim][jprim] * M[iprim][jprim]
 
+            Ns[i][j] = round(np.matmul(T.transpose(), np.matmul(Rs[i][j], T))) + 0.5
+
+    
 
     np.save('transformed_matrix', DCT)
 
