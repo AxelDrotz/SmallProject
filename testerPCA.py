c9da7615e0b6479dab34cf6ffd867a7dc37eb28b
SushiQ <dnordm@kth.se>
2022-01-06 16:35:25 +0100

    New PCA tries

--- testerPCA.py
+++ testerPCA.py
@@ -11,7 +11,7 @@ import time
 def execPCA(A, k):
     pca = PCA(n_components=k)
     R_matrix = pca.fit_transform(A).transpose()
-    print("R_matrix:", R_matrix, "with shape:", R_matrix.shape)
+    #print("R_matrix:", R_matrix, "with shape:", R_matrix.shape)
     return R_matrix
 
 def pca_error(x_matrix, N, d, k):
@@ -32,17 +32,18 @@ def pca_error(x_matrix, N, d, k):
                 pairs.append([i, j])
                 sample_flag = True
                 round += 1
+
         #EXTRACT REFERENCE
-        xi = x_matrix[:, i]
+        xi = x_matrix[:,i]
         xj = x_matrix[:,j]
         x_dist = np.linalg.norm(xi-xj)
 
         # Calculate PCA average error
-        constant = np.sqrt(d/k)
-        #print("r_matrix", r_matrix, r_matrix.shape, "xi", xi, xi.shape)
-        pca_xi = np.matmul(r_matrix, xi)
-        pca_xj = np.matmul(r_matrix, xj)
+        constant = 1#*np.sqrt(d/k)
+        pca_xi = r_matrix[:,i]
+        pca_xj = r_matrix[:,j]
         pca_dist = constant*np.linalg.norm(pca_xi-pca_xj)
+
         pca_average_error += (pca_dist-x_dist)/(x_dist)
 
     return pca_average_error/100, time_elapsed
@@ -65,7 +66,7 @@ dataMatrix = np.load("normalized_array.npy").transpose()
 print("datamatrix:", dataMatrix, dataMatrix.shape)
 
 timeList = []
-k_dimensions = [1,2,3,4,5,6,7,8,9,10,20,30,40,50,60,70,80,90,100,150,200,250,300,400,500,750]
+k_dimensions = [2,3,4,5,6,7,8,9,10,20,30,40,50,60,70,80,90,100,150,200,250,300,400,500,750]
 pca_error_results, time = pca_test(dataMatrix, k_dimensions, timeList)
 print("pca_error_results:", pca_error_results)
 
